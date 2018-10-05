d3e9e7e6220354bd3b45a3c23f3aaa43ce382b7a
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-10-05 10:00:24 -0400

    Works for me but I still have to test it thoroughly.

--- MaxNonNegativeSubArray.py
+++ MaxNonNegativeSubArray.py
@@ -1,55 +1,63 @@
 #!/usr/bin/env python3
 
-def maxset0(A):
-    largestArray = []
-    largestSum = 0
-    for i in range(0, len(A)):
-        for j in range(i+1, len(A)+1):
-            if A[j] > 0:
-                if (sum(A[i:j]) > largestSum):
-                    largestArray = A[i:j]
-                    largestSum = sum(largestArray)
-                    print("sum(A[{},{}])=={}".format(i, j, largestSum))
-                elif (sum(A[i:j]) == largestSum): # There is a tie with sum.
-                    if len(A[i:j]) > len(largestArray):
-                        largestArray = A[i:j]
-                        largestSum = sum(largestArray)
-                    elif len(A[i:j]) < len(largestArray):
-                        pass # Largest is still the same
-                    else: # There is a tie with length.
-                        if A[i] < largestArray[0]:
-                            largestArray = A[i:j]
-                            largestSum = sum(largestArray)
-                        else:
-                            pass
-                else:
-                    pass
-            else:
-                break
-    return(largestArray)
+"""
+Find out the maximum sub-array of non negative numbers from an array.
+The sub-array should be continuous. That is, a sub-array created by choosing
+the second and fourth element and skipping the third element is invalid.
 
-def maxset(A):
-    max = [A[0]]
+Maximum sub-array is defined in terms of the sum of the elements in the
+sub-array. Sub-array A is greater than sub-array B if sum(A) > sum(B).
+
+Example:
+
+A : [1, 2, 5, -7, 2, 3]
+The two sub-arrays are [1, 2, 5] [2, 3].
+The answer is [1, 2, 5] as its sum is larger than [2, 3]
+
+NOTE: If there is a tie, then compare with segment's length and return segment which has maximum length
+NOTE 2: If there is still a tie, then return the segment with minimum starting index
+
+>>> A = [1, 2, 5, -7, 2, 3]
+>>> maxset(A)
+[1, 2, 5]
+
+>>> A = [1, 2, 5, -7, 2, 2, 2, 2]
+>>> maxset(A)
+[2, 2, 2, 2]
+"""
+
+
+def maxset(a_int_l):
+    maximum = [a_int_l[0]]
     i = 0
-    while i < len(A):
+    while i < len(a_int_l):
         j = i
-        while j < len(A)+1:
-            j += 1
-            if A[j] > 0:
-                curr=A[i:j]
-                if sum(max) < sum(curr):  # We just found a new maximum
-                    max = curr
-                elif sum(max) == sum(curr):
-                    if len(max) < len(curr):
-                        max = curr
-                    elif max[i] > curr[i]:
-                        max = curr
+        while j < len(a_int_l):
+            if a_int_l[j] > 0:
+                curr = a_int_l[i:j + 1]
+                if sum(maximum) < sum(curr):  # We just found a new maximum
+                    maximum = curr
+                elif sum(maximum) == sum(curr):
+                    if len(maximum) < len(curr):
+                        maximum = curr
+                    elif maximum[i] > curr[i]:
+                        maximum = curr
             else:
                 break
-        i += 1
+            j += 1
+        i = j + 1
+    return maximum
 
 
+def main():
+    a_int_l = [1, 2, 5, -7, 2, 3]
+    print(maxset(a_int_l))
 
-A = [1, 2, 5, -71, 2, 2, 2, 2]
-B = maxset(A)
-print(B)
+    a_int_l = [1, 2, 5, -7, 2, 2, 2, 2]
+    print(maxset(a_int_l))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
