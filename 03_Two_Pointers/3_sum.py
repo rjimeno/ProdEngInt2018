0463d7a38f86eca63228967f9fb31cb200b7dab4
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-10-13 20:02:40 -0400

    First commit that fully works.

--- 03_Two_Pointers/3_sum.py
+++ 03_Two_Pointers/3_sum.py
@@ -23,8 +23,8 @@ class Solution:
         >>> s.threeSumClosest([1, 2, -1], 2)
         2
 
-        #>>> s.threeSumClosest([-1, 2, 1, -4], 0)
-        #-1
+        >>> s.threeSumClosest([-1, 2, 1, -4], 0)
+        -1
 
         >>> s.threeSumClosest([-1, 2, 1, -4], 1)
         2
@@ -34,40 +34,29 @@ class Solution:
 
         """
         if len(a) < 3:
-            return 1000000000 # Magic number.
+            return 1000000000 # Magic number used by interviewbit's test.
         elif len(a) == 3:
             return sum(a)
         a.sort()
         # The following number represents a magnitude and should never be smaller than zero.
         candidate = None
-        smallest_difference = float('inf')
         for i in range(len(a) - 2):
             top = len(a) - 1
             bottom = i + 1
-            s1 = a[i]
-            s2 = a[top]
-            s3 = a[bottom]
-            total =  s1 + s2 + s3
             while bottom < top:  # and 0 < smallest_difference:
-                total = s1 + s2 + s3
-                if t < total:
-                    if total - t < smallest_difference:
-                        smallest_difference = total - t
+                threeSum = a[i] + a[top] + a[bottom]
+                if candidate is None or abs(t - threeSum) < abs(t - candidate):
+                    candidate = threeSum
+                if t < threeSum:
                     top -= 1
-                    s2 = a[top]
-                    continue
-                elif total < t:
-                    if t - total < smallest_difference:
-                        smallest_difference = t - total
+                elif threeSum < t:
                     bottom += 1
-                    s3 = a[bottom]
-                    continue
                 else:
                     # An exact sum was found and nothing else matters.
                     return t
             # At this point, total != t for every sensible s2 & s3; let's try the next s1.
         # At this point, there was no exact match anywhere; so, let's use the smallest difference instead:
-        return total
+        return candidate
 
 if __name__ == '__main__':
     import doctest
